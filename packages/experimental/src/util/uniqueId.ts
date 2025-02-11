f45b219d634e1c79d27615826f33ebeeddb891b5
Kún Ákos <kunaakos@gmail.com>
2025-02-11 15:25:48 +0100

    fix uuid later :D

--- packages/experimental/src/util/uniqueId.ts
+++ packages/experimental/src/util/uniqueId.ts
@@ -1,3 +1,9 @@
-// TODO: UUID generator - BUT, this is useful for now
+/**
+ * TODO: UUID generator
+ * Pretend this is an actual UUID generator until I write one,
+ * or, even better, figure out licensing for this project
+ * and copy-paste someone else's implementation.
+ * You can pass your own or a lib's anyways!
+ */
 export const uniqueId = () =>
     `${Date.now()}_${Math.floor(Math.random() * 1000)}`
