fcc24e9639f21baac9537411f076f712f94e9577
Kún Ákos <kunaakos@gmail.com>
2025-02-12 13:00:23 +0100

    don't allow uplinks to block dispatch calls

--- packages/experimental/src/messaging.ts
+++ packages/experimental/src/messaging.ts
@@ -6,7 +6,8 @@ import {
 } from 'src/types/messaging'
 import { Downlink, LinkFn, Uplink } from 'src/types/remoting'
 import { Actor, DispatchFn } from 'src/types/system'
-import { eventually } from './util/eventually'
+
+import { eventually } from 'src/util/eventually'
 
 export const initMessaging = ({
     systemId,
@@ -16,11 +17,17 @@ export const initMessaging = ({
     const locals: Record<ActorId, Actor> = {}
     let uplink: Nullable<Uplink> = null
 
+    /**
+     * NOTE: `eventually` and non-blocking calls were not
+     * throroughly though out. Should revisit this.
+     * The idea is that `dispatch` calls should never block anything.
+     * The supervisor handles the same for locally dispatched messages.
+     */
     const dispatch: DispatchFn = (message) => {
         if (locals[message.meta.to]) {
             locals[message.meta.to]?.dispatch(message)
         } else if (uplink) {
-            eventually(() => uplink && uplink.dispatch(message))
+            eventually(() => uplink && uplink.dispatch(message))()
         } else {
             // TODO: handle messages without recipients
         }
