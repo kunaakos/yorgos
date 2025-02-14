adbba7fe50568ddb60cad9c88eb9d106d1508607
Ákos Kún <kunaakos@gmail.com>
2025-02-14 10:50:04 +0100

    update and extend relevant tests

--- packages/experimental/src/system.test.ts
+++ packages/experimental/src/system.test.ts
@@ -23,11 +23,21 @@ type TestMessageWithPayload = PlainMessage<
 
 type TestQueryMessage = QueryMessage<
     'TEST_QUERY', //
+    null //
+>
+
+type TestQueryMessageWithPayload = QueryMessage<
+    'TEST_QUERY_WITH_PAYLOAD', //
     { string: string } //
 >
 
 type TestResponseMessage = ResponseMessage<
     'TEST_RESPONSE', //
+    null //
+>
+
+type TestResponseMessageWithPayload = ResponseMessage<
+    'TEST_RESPONSE_WITH_PAYLOAD', //
     { string: string } //
 >
 
@@ -85,14 +95,14 @@ describe('actor system', () => {
         const TEST_ACTOR_ID = 'TEST_ACTOR'
         const TEST_SYSTEM_ID = 'TEST_SYSTEM'
 
-        const actorFn: ActorFn<null, TestQueryMessage> = async ({
+        const actorFn: ActorFn<null, TestQueryMessageWithPayload> = async ({
             msg,
             dispatch,
         }) => {
-            if (msg.type === 'TEST_QUERY') {
+            if (msg.type === 'TEST_QUERY_WITH_PAYLOAD') {
                 messageLog.push(JSON.stringify(msg))
-                const testResponseMessage: TestResponseMessage = {
-                    type: 'TEST_RESPONSE',
+                const testResponseMessage: TestResponseMessageWithPayload = {
+                    type: 'TEST_RESPONSE_WITH_PAYLOAD',
                     payload: { string: 'test response' },
                     meta: responseMetaTo(msg.meta),
                 }
@@ -112,18 +122,21 @@ describe('actor system', () => {
         })
 
         const { type: responseType, payload: responsepayload } =
-            await system.query<TestQueryMessage, TestResponseMessage>({
+            await system.query<
+                TestQueryMessageWithPayload,
+                TestResponseMessageWithPayload
+            >({
                 id: TEST_ACTOR_ID,
-                type: 'TEST_QUERY',
+                type: 'TEST_QUERY_WITH_PAYLOAD',
                 payload: { string: 'test query' },
             })
 
-        expect(responseType).toBe('TEST_RESPONSE')
+        expect(responseType).toBe('TEST_RESPONSE_WITH_PAYLOAD')
         expect(responsepayload).toStrictEqual({ string: 'test response' })
         expect(messageLog).toHaveLength(1)
         expect(unexpectedMessageLog).toHaveLength(0)
         expect(JSON.parse(messageLog[0] as string)).toStrictEqual({
-            type: 'TEST_QUERY',
+            type: 'TEST_QUERY_WITH_PAYLOAD',
             payload: { string: 'test query' },
             meta: {
                 id: 'MOCK_ID_1',
@@ -141,15 +154,15 @@ describe('actor system', () => {
         const TEST_ACTOR_ID = 'TEST_ACTOR'
         const TEST_SYSTEM_ID = 'TEST_SYSTEM'
 
-        const actorFn: ActorFn<null, TestQueryMessage> = async ({
+        const actorFn: ActorFn<null, TestQueryMessageWithPayload> = async ({
             msg,
             dispatch,
         }) => {
-            if (msg.type === 'TEST_QUERY') {
+            if (msg.type === 'TEST_QUERY_WITH_PAYLOAD') {
                 messageLog.push(JSON.stringify(msg))
                 await delay(10)
-                const testResponseMessage: TestResponseMessage = {
-                    type: 'TEST_RESPONSE',
+                const testResponseMessage: TestResponseMessageWithPayload = {
+                    type: 'TEST_RESPONSE_WITH_PAYLOAD',
                     payload: { string: 'test response' },
                     meta: responseMetaTo(msg.meta),
                 }
@@ -169,20 +182,23 @@ describe('actor system', () => {
         })
 
         expect(
-            system.query<TestQueryMessage, TestResponseMessage>({
+            system.query<
+                TestQueryMessageWithPayload,
+                TestResponseMessageWithPayload
+            >({
                 id: TEST_ACTOR_ID,
-                type: 'TEST_QUERY',
+                type: 'TEST_QUERY_WITH_PAYLOAD',
                 payload: { string: 'test query' },
                 options: { timeout: 5 },
             }),
         ).rejects.toThrow('Query timed out.')
 
-        await delay(15)
+        await delay(15) // NOTE: replace timeouts with better promise-based control
 
         expect(messageLog).toHaveLength(1)
         expect(unexpectedMessageLog).toHaveLength(0)
         expect(JSON.parse(messageLog[0] as string)).toStrictEqual({
-            type: 'TEST_QUERY',
+            type: 'TEST_QUERY_WITH_PAYLOAD',
             payload: { string: 'test query' },
             meta: {
                 id: 'MOCK_ID_1',
@@ -193,7 +209,85 @@ describe('actor system', () => {
         })
     })
 
-    test('async execution order', async () => {
+    test('actor concurrency', async () => {
+        const eventLog: string[] = []
+        const queries: Promise<any>[] = []
+
+        const TEST_SYSTEM_ID = 'TEST_SYSTEM'
+
+        const actorFn: ActorFn<null, TestMessage> = () => {
+            eventLog.push('A')
+            return null
+        }
+
+        const asyncActorFn: ActorFn<null, TestMessage> = async () => {
+            eventLog.push('B')
+            return null
+        }
+
+        const slowActorFn: ActorFn<null, TestQueryMessageWithPayload> = async ({
+            msg,
+            dispatch,
+        }) => {
+            await delay(5)
+            eventLog.push('C')
+            const testResponseMessage: TestResponseMessage = {
+                type: 'TEST_RESPONSE',
+                payload: null,
+                meta: responseMetaTo(msg.meta),
+            }
+            dispatch(testResponseMessage)
+            return null
+        }
+
+        const system = initSystem({ id: TEST_SYSTEM_ID })
+        system.spawn({
+            id: '1',
+            fn: actorFn,
+            initialState: null,
+        })
+        system.spawn({
+            id: '2',
+            fn: asyncActorFn,
+            initialState: null,
+        })
+        system.spawn({
+            id: '3',
+            fn: slowActorFn,
+            initialState: null,
+        })
+
+        for (let i = 0; i <= 8; i++) {
+            const actorIndex = i % 3
+            const actorId = `${actorIndex + 1}`
+            if (actorIndex === 2) {
+                queries.push(
+                    system.query<TestQueryMessage, TestResponseMessage>({
+                        id: actorId,
+                        type: 'TEST_QUERY',
+                        payload: null,
+                    }),
+                )
+            } else {
+                system.dispatch({
+                    type: 'TEST_MESSAGE',
+                    payload: null,
+                    meta: plainMeta({
+                        to: actorId,
+                    }),
+                })
+            }
+        }
+
+        eventLog.push('S') // "Start"
+        await Promise.all(queries) // none should reject
+        eventLog.push('E') // "End"
+
+        expect(eventLog).toHaveLength(11)
+        expect(eventLog.join('')).toBe('SABABABCCCE')
+    })
+
+    test('message processing concurrency', async () => {
         const messageLog: string[] = []
         const unexpectedMessageLog: string[] = []
         const eventLog: string[] = []
@@ -201,26 +295,26 @@ describe('actor system', () => {
         const TEST_ACTOR_ID = 'TEST_ACTOR'
         const TEST_SYSTEM_ID = 'TEST_SYSTEM'
 
-        const actorFn: ActorFn<null, TestQueryMessage | TestMessage> = async ({
-            msg,
-            dispatch,
-        }) => {
+        const actorFn: ActorFn<
+            null,
+            TestQueryMessageWithPayload | TestMessage
+        > = async ({ msg, dispatch }) => {
             if (msg.type === 'TEST_MESSAGE') {
                 messageLog.push(JSON.stringify(msg))
-                eventLog.push('4: first message processed')
+                eventLog.push('4: plain message processed')
                 return null
-            } else if (msg.type === 'TEST_QUERY') {
+            } else if (msg.type === 'TEST_QUERY_WITH_PAYLOAD') {
                 messageLog.push(JSON.stringify(msg))
                 if (msg.payload.string === 'slow') {
                     eventLog.push('5: slow query starts processing')
-                    await delay(10)
+                    await delay(5)
                     eventLog.push('6: slow query finishes processing')
                 }
                 if (msg.payload.string === 'fast') {
-                    eventLog.push('7: fast query processed')
+                    eventLog.push('8: fast query processed')
                 }
-                const testResponseMessage: TestResponseMessage = {
-                    type: 'TEST_RESPONSE',
+                const testResponseMessage: TestResponseMessageWithPayload = {
+                    type: 'TEST_RESPONSE_WITH_PAYLOAD',
                     payload: { string: `${msg.payload.string} response` },
                     meta: responseMetaTo(msg.meta),
                 }
@@ -250,36 +344,36 @@ describe('actor system', () => {
         eventLog.push('1: plain message dispatched')
 
         const slowResponsePromise = system.query<
-            TestQueryMessage,
-            TestResponseMessage
+            TestQueryMessageWithPayload,
+            TestResponseMessageWithPayload
         >({
             id: TEST_ACTOR_ID,
-            type: 'TEST_QUERY',
+            type: 'TEST_QUERY_WITH_PAYLOAD',
             payload: { string: 'slow' },
         })
         eventLog.push('2: slow query message dispatched')
 
         const fastResponsePromise = system.query<
-            TestQueryMessage,
-            TestResponseMessage
+            TestQueryMessageWithPayload,
+            TestResponseMessageWithPayload
         >({
             id: TEST_ACTOR_ID,
-            type: 'TEST_QUERY',
+            type: 'TEST_QUERY_WITH_PAYLOAD',
             payload: { string: 'fast' },
         })
         eventLog.push('3: fast query message dispatched')
 
         const { type: slowResponseType, payload: slowResponsePayload } =
             await slowResponsePromise
-        eventLog.push('8: slow query response received')
+        eventLog.push('7: slow query response received')
 
         const { type: fastResponseType, payload: fastResponsePayload } =
             await fastResponsePromise
         eventLog.push('9: fast query response received')
 
-        expect(slowResponseType).toBe('TEST_RESPONSE')
+        expect(slowResponseType).toBe('TEST_RESPONSE_WITH_PAYLOAD')
         expect(slowResponsePayload).toStrictEqual({ string: 'slow response' })
-        expect(fastResponseType).toBe('TEST_RESPONSE')
+        expect(fastResponseType).toBe('TEST_RESPONSE_WITH_PAYLOAD')
         expect(fastResponsePayload).toStrictEqual({ string: 'fast response' })
 
         expect(messageLog).toHaveLength(3)
@@ -293,7 +387,7 @@ describe('actor system', () => {
             },
         })
         expect(JSON.parse(messageLog[1] as string)).toStrictEqual({
-            type: 'TEST_QUERY',
+            type: 'TEST_QUERY_WITH_PAYLOAD',
             payload: { string: 'slow' },
             meta: {
                 id: 'MOCK_ID_2',
@@ -303,7 +397,7 @@ describe('actor system', () => {
             },
         })
         expect(JSON.parse(messageLog[2] as string)).toStrictEqual({
-            type: 'TEST_QUERY',
+            type: 'TEST_QUERY_WITH_PAYLOAD',
             payload: { string: 'fast' },
             meta: {
                 id: 'MOCK_ID_4',
@@ -317,11 +411,11 @@ describe('actor system', () => {
             '1: plain message dispatched',
             '2: slow query message dispatched',
             '3: fast query message dispatched',
-            '4: first message processed',
+            '4: plain message processed',
             '5: slow query starts processing',
             '6: slow query finishes processing',
-            '7: fast query processed',
-            '8: slow query response received',
+            '7: slow query response received',
+            '8: fast query processed',
             '9: fast query response received',
         ])
     })
